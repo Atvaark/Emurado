fe6d4d10f94859ba810783ca1b4095f2926c8e41
Atvaark <Atvaark@users.noreply.github.com>
2015-04-10 16:37:43 +0200

    Added chat channel types

---
+++ HaloOnline.Server.Model/Messaging/ChannelTypes.cs
@@ -0,0 +1,13 @@
+﻿namespace HaloOnline.Server.Model.Messaging
+{
+    public static class ChannelTypes
+    {
+        public const string Clan = "#clan_{0}";
+        public const string Game = "#game_{0}";
+        public const string Gameteam = "#gameteam_{0}";
+        public const string General = "#general_{0}";
+        public const string Party = "#party_{0}";
+        public const string Private = "#private_{0}";
+
+    }
+}
